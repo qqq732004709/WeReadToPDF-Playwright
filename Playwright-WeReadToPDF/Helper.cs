ee61fa0a994a027e7343c0125598fcddeb1e8272
Xiaoyu <732004709@qq.com>
2022-10-17 18:18:00 +0800

    img loaded check

--- Playwright-WeReadToPDF/Helper.cs
+++ Playwright-WeReadToPDF/Helper.cs
@@ -23,6 +23,7 @@ public class Helper
             for (int i = 0; i < waitTurns; i++)
             {
                 Console.WriteLine($"Wait for QRCode Scan...{i}/{waitTurns}turns");
+                await page.WaitForTimeoutAsync(3000);
                 var content = await page.QuerySelectorAsync(".menu_container");
                 if (content != null)
                 {
@@ -30,12 +31,67 @@ public class Helper
                     break;
                 }
 
-                await page.WaitForTimeoutAsync(1000);
             }
         }
         catch (Exception)
         {
             throw new Exception("Login Time Out!");
         }
+    }
+
+    public async Task SaveAsPDF(string bookUrl)
+    {
+        if (bookUrl.IndexOf("weread.qq.com/web/reader") < 0)
+        {
+            throw new Exception("Wrong Url");
+        }
+
+        await page.GotoAsync(bookUrl);
+
+        await page.ClickAsync("button.catalog");
+        await page.ClickAsync("li.chapterItem:nth-child(2)");
+
+        var bookName = page.TextContentAsync("span.readerTopBar_title_link");
+        Console.WriteLine($"ready to scan {bookName}");
+
+        var pageIndex = 1;
+        while (true)
+        {
+            Thread.Sleep(500);
+
+            var chapter = await page.TextContentAsync("span.readerTopBar_title_chapter");
+            Console.WriteLine($"scanning chapter{chapter}");
+
+            await CheckAllImageLoaded();
+        }
+
+    }
+
+    public async Task<bool> CheckAllImageLoaded()
+    {
+        var unCheckedImg = (List<IElementHandle?>)await page.QuerySelectorAllAsync("img.wr_absolute");
+        if (unCheckedImg == null || unCheckedImg.Count == 0)
+        {
+            Console.WriteLine("all img loaded");
+            return true;
+        }
+
+        for (int i = 0; i < 100; i++)
+        {
+            await page.WaitForTimeoutAsync(50);
+            foreach (var img in unCheckedImg)
+            {
+                var prop = img.GetPropertyAsync("complete");
+                unCheckedImg.Remove(img);
+            }
+            if (unCheckedImg.Count == 0)
+            {
+                Console.WriteLine("all img loaded");
+                return true;
+            }
+        }
+
+        Console.WriteLine("all img not loaded");
+        return false;
     }
 }
--- Playwright-WeReadToPDF/Program.cs
+++ Playwright-WeReadToPDF/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Playwright_WeReadToPDF;
+
+using var playwright = await Playwright.CreateAsync();
+await using var browser = await playwright.Chromium.LaunchAsync(new() { Channel = "msedge", Headless = false });
+var page = await browser.NewPageAsync();
+
+var helper = new Helper(page);
+await helper.Login();
+await helper.SaveAsPDF("https://weread.qq.com/web/reader/a3d3227071db5702a3d9c37");
+await browser.CloseAsync();
